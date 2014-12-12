98624148aa5c9127d79d7f64df3409d2bf92fb90
timduru <timduru@gmail.com>
2014-12-12 17:44:14 +0100

    cleanups

--- filesystem_overlay/overlay.mk
+++ filesystem_overlay/overlay.mk
@@ -1,6 +1,5 @@
 
 PRODUCT_COPY_FILES += \
-    vendor/eos/filesystem_overlay/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
     vendor/eos/filesystem_overlay/bin/sysinit:system/bin/sysinit \
     vendor/eos/filesystem_overlay/bin/sysrw:system/bin/sysrw \
     vendor/eos/filesystem_overlay/bin/sysro:system/bin/sysro \
@@ -10,7 +9,6 @@ PRODUCT_COPY_FILES += \
     vendor/eos/filesystem_overlay/etc/init.d/02_privacy_controls:system/etc/init.d/02_privacy_controls \
     vendor/eos/filesystem_overlay/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/eos/filesystem_overlay/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/eos/filesystem_overlay/addon.d/99-eos-hosts.sh:system/addon.d/99-eos-hosts.sh \
     vendor/eos/filesystem_overlay/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/eos/filesystem_overlay/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
     vendor/eos/filesystem_overlay/init.kat.rc:root/init.kat.rc \
