d244e6acbcecad1e15335b788a427bb45be9ec90
timduru <timduru@gmail.com>
2016-07-28 11:17:55 +0200

    keep under device tree for easier maintenance

--- tf201/BoardConfig.mk
+++ tf201/BoardConfig.mk
@@ -1,13 +1,1 @@
-include device/asus/tf_unified/BoardConfig_common.mk
-
-
-# Try to build the kernel
-TARGET_KERNEL_SOURCE := kernel/asus/tf201
-TARGET_KERNEL_CONFIG := katkernel_defconfig
-
-# Prebuilt Kernel Fallback
-TARGET_PREBUILT_KERNEL := device/asus/tf_unified/kernel
-
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= vendor/kat/tf201/bluetooth
-
-
+-include device/asus/tf_unified/BoardConfig_common.mk
--- tf700t/BoardConfig.mk
+++ tf700t/BoardConfig.mk
@@ -1,11 +1,1 @@
-include device/asus/tf_unified/BoardConfig_common.mk
-
-
-# Try to build the kernel
-TARGET_KERNEL_SOURCE := kernel/asus/tf700t
-TARGET_KERNEL_CONFIG := katkernel_defconfig
-
-# Prebuilt Kernel Fallback
-TARGET_PREBUILT_KERNEL := device/asus/tf_unified/kernel
-
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= vendor/kat/tf700t/bluetooth
+-include device/asus/tf_unified/BoardConfig_common.mk
